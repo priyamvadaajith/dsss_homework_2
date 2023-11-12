67f77984509cb3fdaa95547249961daa5600b790
priyamvadaajith <priyamvadaajithmenon@gmail.com>
2023-11-12 01:28:57 +0100

    Add setup.py and make project pip installable

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='math_quiz',  # Replace with your project name
+    version='0.1',  # Replace with your project version
+    packages=find_packages(),
+    install_requires=[
+        # List your dependencies here
+    ],
+)
